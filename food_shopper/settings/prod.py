69b22ad8f0a8352bdfbcf8b8920a30f344644a36
Kaine Bruce <kaineyb@gmail.com>
2022-05-12 16:23:06 +0100

    added cors

--- food_shopper/settings/prod.py
+++ food_shopper/settings/prod.py
@@ -7,3 +7,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 ALLOWED_HOSTS = [
     "food-shopper-api.herokuapp.com"
 ]  # will update once we pick a platform
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://food-shopper-kjb.herokuapp.com/",
+]
