98ada01d1cbbf25b482cceff0aa2f866d12a9109
Kaine Bruce <kaineyb@gmail.com>
2022-05-12 16:02:18 +0100

    updated allowed hosts

--- food_shopper/settings/prod.py
+++ food_shopper/settings/prod.py
@@ -5,5 +5,5 @@ from .common import *
 DEBUG = False
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALLOWED_HOSTS = [
-    "https://food-shopper-api.herokuapp.com/"
+    "food-shopper-api.herokuapp.com"
 ]  # will update once we pick a platform
