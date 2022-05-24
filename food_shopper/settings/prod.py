846dbf5e955c83832ff39a3f9f216482e2fd1160
Kaine Bruce <kaineyb@gmail.com>
2022-05-24 13:16:58 +0100

    tidied imports

--- food_shopper/settings/prod.py
+++ food_shopper/settings/prod.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from .common import *
 
 DEBUG = False
@@ -13,6 +15,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://food-shopper-kjb.herokuapp.com",
 ]
 
-import dj_database_url
 
+DATABASES = {}
 DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
