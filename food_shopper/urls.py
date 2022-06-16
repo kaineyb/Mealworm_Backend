a9bf95bb04c70914ba6a1056d964d89e77d35958
Kaine Bruce <kaineyb@gmail.com>
2022-06-16 15:32:37 +0100

    added index.html

--- food_shopper/urls.py
+++ food_shopper/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 import debug_toolbar
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("shopping/", include("shopping.urls")),
     path("auth/", include("djoser.urls")),
