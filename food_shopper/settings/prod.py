1b05fee7a139129e743bc531c55001ff267ad393
Kaine Bruce <kaineyb@gmail.com>
2026-01-26 21:17:44 +0000

    trying render

--- food_shopper/settings/prod.py
+++ food_shopper/settings/prod.py
@@ -1,26 +1,30 @@
 import os
 import secrets
 
+import dj_database_url
+
 from .common import *
 
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY") or secrets.token_urlsafe(16)
-ALLOWED_HOSTS = ["api.mealworm.uk"]  # will update once we pick a platform
 
+ALLOWED_HOSTS = [
+    "api.mealworm.uk",
+    ".onrender.com",
+]
 
 CORS_ALLOWED_ORIGINS = [
     "https://mealworm.uk",
 ]
 
+# Allow Render preview URLs in CORS
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.onrender\.com$",
+]
 
 DATABASES = {
-    "default": {
-        "NAME": os.environ["DATABASE_NAME"],
-        "ENGINE": "django.db.backends.mysql",
-        "USER": os.environ["DATABASE_USER"],
-        "PASSWORD": os.environ["DATABASE_PASS"],
-        "OPTIONS": {
-            "autocommit": True,
-        },
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
